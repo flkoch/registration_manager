16096d5441c5f4c8f9acc079addd70f717315502
Florian Koch <flkoch@dev.fkoch.ch>
2020-12-05 12:51:00 +0100

    Added static files configuration and excluded those statically collected files from being tracked

--- registration_manager/registration_manager/settings.py
+++ registration_manager/registration_manager/settings.py
@@ -130,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = env.path('STATIC_ROOT')
+STATICFILES_DIRS = env.list('STATICFILES_DIRS')
