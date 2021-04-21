ff7f9f71d1326180a61395b4d1dabf5a43f1ee1e
Florian Koch <flkoch@dev.fkoch.ch>
2021-04-21 18:40:10 +0200

    Resolved merge conflicts

--- registration_manager/urls.py
+++ registration_manager/urls.py
@@ -15,19 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-<<<<<<< HEAD
 from django.conf import settings
 from django.conf.urls.static import static
-=======
->>>>>>> e7a46abab3bc963b6fbd397edae31c433a8ef4d3
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-<<<<<<< HEAD
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-=======
->>>>>>> e7a46abab3bc963b6fbd397edae31c433a8ef4d3
